329071e68110e812190a520760c7d8e3a02718e7
Mikhail Tovt <mikhail.tovt.02@mail.ru>
2022-09-12 14:48:56 +0300

    FRONT: сделать статистику доски

--- src/components/Board/BoardStatsItem.vue
+++ src/components/Board/BoardStatsItem.vue
@@ -1,7 +1,16 @@
 <template>
-  <div>
-    {{ member.user_name }} - {{ cost }}
-  </div>
+  <tr>
+    <td>
+      <div class="content">
+        {{ member.user_name }}
+      </div>
+    </td>
+    <td>
+      <div class="content">
+        {{ cost }}
+      </div>
+    </td>
+  </tr>
 </template>
 
 <script>
@@ -30,8 +39,62 @@ export default {
         }
         return valString
       }
-      return ''
+      return 0
     }
   }
 }
 </script>
+
+<style scoped>
+.content {
+  @apply min-h-[48px] flex items-center
+}
+
+tr:first-child {
+  @apply text-left h-[48px]
+}
+
+tr:first-child th {
+  @apply pl-[20px] border-[#e6e6e6] border-2 text-[14px] font-medium;
+  border-spacing: 0 20px;
+}
+
+tr:first-child th:first-child {
+  @apply rounded-l-[6px]
+}
+
+tr:first-child th:last-child {
+  @apply rounded-r-[6px]
+}
+
+tr:first-child th:not(:last-child){
+  border-right: 1px;
+}
+
+/*Стили для контентной части*/
+
+tr:not(:first-child) {
+  @apply min-h-[58px] align-top
+}
+
+tr:not(:first-child) td {
+  @apply border-b-2 border-[#e6e6e6] pl-[14px] font-normal text-[13px] max-w-[400px]
+}
+
+tr:not(:first-child) td:first-child {
+  @apply rounded-l-[6px]
+}
+
+tr:not(:first-child) td:last-child {
+  @apply rounded-r-[6px]
+}
+
+/*Стили для первого контентного элемента (отступ сверху)*/
+tr:nth-child(2) {
+  @apply min-h-[74px]
+}
+
+tr:nth-child(2) .content {
+  @apply mt-[16px]
+}
+</style>
--- src/components/BoardStats.vue
+++ src/components/BoardStats.vue
@@ -1,14 +1,20 @@
 <template>
   <div class="mt-5">
     <p>Статистика доски {{ boardName }}</p>
-    <template
-      v-for="(member, index) in membersByCost"
-      :key="index"
-    >
-      <BoardStatsItem
-        :member="member"
-      />
-    </template>
+    <table>
+      <tr>
+        <th>Сотрудник</th>
+        <th>кол-во/деньги</th>
+      </tr>
+      <template
+        v-for="(member, index) in membersByCost"
+        :key="index"
+      >
+        <BoardStatsItem
+          :member="member"
+        />
+      </template>
+    </table>
   </div>
 </template>
 
@@ -55,7 +61,7 @@ export default {
             if (this.membersByCost[card.user]) {
               this.membersByCost[card.user].cost += card.cost
             } else {
-              if (card.user !== '') {
+              if (card.user !== '' && this.employeesByEmail[card.user]?.name) {
                 this.membersByCost[card.user] =
                   {
                     user_email: card.user,
@@ -71,3 +77,63 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+    table {
+  @apply w-[calc(100%-40px)] mx-[20px] mt-[20px] border-separate border-spacing-0
+}
+
+/*Стили для первой строки с заголовками*/
+
+tr:first-child {
+  @apply text-left h-[48px]
+}
+
+tr:first-child th {
+  @apply pl-[20px] border-[#e6e6e6] border-2 text-[14px] font-medium;
+  border-spacing: 0 20px;
+}
+
+tr:first-child th:first-child {
+  @apply rounded-l-[6px]
+}
+
+tr:first-child th:last-child {
+  @apply rounded-r-[6px]
+}
+
+tr:first-child th:not(:last-child){
+  border-right: 1px;
+}
+
+/*Стили для контентной части*/
+
+tr:not(:first-child) {
+  @apply min-h-[58px] align-top
+}
+
+tr:not(:first-child) td {
+  @apply border-b-2 border-[#e6e6e6] pl-[14px] font-normal text-[13px] max-w-[400px]
+}
+
+tr:not(:first-child) td:first-child {
+  @apply rounded-l-[6px]
+}
+
+tr:not(:first-child) td:last-child {
+  @apply rounded-r-[6px]
+}
+
+.content {
+  @apply min-h-[48px] flex items-center
+}
+
+/*Стили для первого контентного элемента (отступ сверху)*/
+tr:nth-child(2) {
+  @apply min-h-[74px]
+}
+
+tr:nth-child(2) .content {
+  @apply mt-[16px]
+}
+</style>
