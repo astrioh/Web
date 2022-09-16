854e04af42dc4547b908cc6f32e760d6ab34b1b5
Dmitry Gashilov <gashilovdmitry@yandex.ru>
2022-09-16 14:48:37 +0300

    добавил скелетон на загрузку клиентов

--- src/components/Clients/Clients.vue
+++ src/components/Clients/Clients.vue
@@ -19,9 +19,6 @@
         <th>Email</th>
         <th>Комментарий</th>
       </tr>
-      <template v-if="status === 'loading'">
-        We are loading clients for you...
-      </template>
       <template v-if="status === 'success'">
         <tr
           v-for="client in clients"
@@ -59,6 +56,120 @@
         </tr>
       </template>
     </table>
+
+    <template v-if="status === 'loading'">
+      <div
+        role="status"
+        class="p-4 animate-pulse dark:divide-gray-700 md:p-6"
+      >
+        <div class="flex justify-between items-center">
+          <div>
+            <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5" />
+            <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700" />
+          </div>
+          <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12" />
+        </div>
+        <div class="flex justify-between items-center pt-4">
+          <div>
+            <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5" />
+            <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700" />
+          </div>
+          <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12" />
+        </div>
+        <div class="flex justify-between items-center pt-4">
+          <div>
+            <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5" />
+            <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700" />
+          </div>
+          <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12" />
+        </div>
+        <div class="flex justify-between items-center pt-4">
+          <div>
+            <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5" />
+            <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700" />
+          </div>
+          <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12" />
+        </div>
+        <div class="flex justify-between items-center pt-4">
+          <div>
+            <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5" />
+            <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700" />
+          </div>
+          <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12" />
+        </div>
+        <div class="flex justify-between items-center">
+          <div>
+            <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5" />
+            <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700" />
+          </div>
+          <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12" />
+        </div>
+        <div class="flex justify-between items-center pt-4">
+          <div>
+            <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5" />
+            <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700" />
+          </div>
+          <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12" />
+        </div>
+        <div class="flex justify-between items-center pt-4">
+          <div>
+            <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5" />
+            <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700" />
+          </div>
+          <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12" />
+        </div>
+        <div class="flex justify-between items-center pt-4">
+          <div>
+            <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5" />
+            <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700" />
+          </div>
+          <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12" />
+        </div>
+        <div class="flex justify-between items-center pt-4">
+          <div>
+            <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5" />
+            <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700" />
+          </div>
+          <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12" />
+        </div>
+        <div class="flex justify-between items-center">
+          <div>
+            <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5" />
+            <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700" />
+          </div>
+          <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12" />
+        </div>
+        <div class="flex justify-between items-center pt-4">
+          <div>
+            <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5" />
+            <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700" />
+          </div>
+          <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12" />
+        </div>
+        <div class="flex justify-between items-center pt-4">
+          <div>
+            <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5" />
+            <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700" />
+          </div>
+          <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12" />
+        </div>
+        <div class="flex justify-between items-center pt-4">
+          <div>
+            <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5" />
+            <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700" />
+          </div>
+          <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12" />
+        </div>
+        <div class="flex justify-between items-center pt-4">
+          <div>
+            <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5" />
+            <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700" />
+          </div>
+          <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12" />
+        </div>
+        <span class="sr-only">Loading...</span>
+      </div>
+    </template>
     <div
       class="group flex justify-center border border-[#0000001F] rounded-[7px] p-[7px] mt-[15px] cursor-pointer"
       @click="clickAddClient"
