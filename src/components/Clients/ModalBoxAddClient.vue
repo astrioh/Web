fb703e707ed3be50dc43ccc88d4237947c229592
Vadim Ekimov <ekimov_vd@mail.ru>
2022-09-19 20:11:11 +0700

    добавил валидацию телефона при создании клиента

--- src/components/Clients/ModalBoxAddClient.vue
+++ src/components/Clients/ModalBoxAddClient.vue
@@ -7,27 +7,48 @@
     @cancel="onCancel"
   >
     <div class="flex flex-col w-full">
-      <span class="mb-1">Имя</span>
-      <input
-        ref="inputValue"
-        v-model="name"
-        type="text"
-        :maxLength="maxLengthInput"
-        class="bg-[#f4f5f7]/50 mb-3 rounded-[6px] focus:ring-0 border border-[#4c4c4d] focus:border-[#ff9123] w-full px-[14px] py-[11px] text-[14px] leading-[16px] text-[#4c4c4d] font-roboto"
-      >
-      <span class="mb-1">Телефон</span>
-      <input
-        v-model="phone"
-        type="text"
-        :maxLength="maxLengthInput"
-        class="bg-[#f4f5f7]/50 mb-3 rounded-[6px] focus:ring-0 border border-[#4c4c4d] focus:border-[#ff9123] w-full px-[14px] py-[11px] text-[14px] leading-[16px] text-[#4c4c4d] font-roboto"
-      >
-      <span class="mb-1">Email</span>
-      <input
-        v-model="email"
-        type="text"
-        class="bg-[#f4f5f7]/50 mb-3 rounded-[6px] focus:ring-0 border border-[#4c4c4d] focus:border-[#ff9123] w-full px-[14px] py-[11px] text-[14px] leading-[16px] text-[#4c4c4d] font-roboto"
-      >
+      <div class="mb-3">
+        <span class="mb-1">Имя</span>
+        <input
+          ref="inputValue"
+          v-model="name"
+          type="text"
+          :maxLength="maxLengthInput"
+          class="bg-[#f4f5f7]/50 rounded-[6px] focus:ring-0 border w-full px-[14px] py-[11px] text-[14px] leading-[16px] text-[#4c4c4d] font-roboto"
+          :class="onValidateField('name')"
+        >
+        <span
+          v-if="!validateNameField"
+          class="text-[11px] text-[#dc2626]"
+        >Обязательное для заполнения</span>
+      </div>
+      <div class="mb-3">
+        <span class="mb-1">Телефон</span>
+        <input
+          v-model="phone"
+          type="text"
+          :maxLength="maxLengthInput"
+          class="bg-[#f4f5f7]/50 rounded-[6px] focus:ring-0 border w-full px-[14px] py-[11px] text-[14px] leading-[16px] text-[#4c4c4d] font-roboto"
+          :class="onValidateField('phone')"
+        >
+        <span
+          v-if="!validatePhoneField"
+          class="text-[11px] text-[#dc2626]"
+        >Обязательное для заполнения</span>
+      </div>
+      <div class="mb-3">
+        <span class="mb-1">Email</span>
+        <input
+          v-model="email"
+          type="text"
+          class="bg-[#f4f5f7]/50 rounded-[6px] focus:ring-0 w-full px-[14px] py-[11px] text-[14px] leading-[16px] text-[#4c4c4d] font-roboto border"
+          :class="onValidateField('email')"
+        >
+        <span
+          v-if="!validateEmailField"
+          class="text-[11px] text-[#dc2626]"
+        >Обязательное для заполнения</span>
+      </div>
       <span class="mb-1">Комментарий</span>
       <input
         v-model="comment"
@@ -72,7 +93,24 @@ export default {
       return '50'
     },
     buttonSaveDisabled () {
-      return !this.name || !this.phone || !this.email || !this.comment
+      return !this.validateNameField || !this.validatePhoneField || !this.validateEmailField
+    },
+    validateNameField () {
+      const minNameLength = 0
+      const maxNameLength = 50
+
+      return this.name.length > minNameLength && this.name.length < maxNameLength
+    },
+    validatePhoneField () {
+      // eslint-disable-next-line no-useless-escape
+      return this.phone.toLowerCase().match(/^(\+7|7|8)?[\s\-]?\(?[489][0-9]{2}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$/)
+    },
+    validateEmailField () {
+      return String(this.email)
+        .toLowerCase()
+        .match(
+          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        )
     }
   },
   methods: {
@@ -95,6 +133,21 @@ export default {
       this.phone = ''
       this.email = ''
       this.comment = ''
+    },
+    onValidateField (key) {
+      const defaultClass = 'border-[#4c4c4d] focus:border-[#ff9123]'
+      const errorClass = 'border-rose-500 focus:border-rose-500'
+
+      switch (key) {
+        case 'name':
+          return !this.validateNameField ? errorClass : defaultClass
+        case 'phone':
+          return !this.validatePhoneField ? errorClass : defaultClass
+        case 'email':
+          return !this.validateEmailField ? errorClass : defaultClass
+        default:
+          return defaultClass
+      }
     }
   }
 }
