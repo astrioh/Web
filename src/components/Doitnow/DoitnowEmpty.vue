859ba030723b2de6205e3263d1fe23deeda84a2e
Egor Nikitin yzk-jssc <egonik1337@gmail.com>
2022-09-21 11:47:49 +0300

    правки стилей кнопок в пустой очереди и пройти тест

--- src/components/Doitnow/DoitnowEmpty.vue
+++ src/components/Doitnow/DoitnowEmpty.vue
@@ -18,7 +18,7 @@
         <div>
           <router-link to="/tasks/today">
             <button
-              class="bg-[#FF912380] px-[18px] py-[16px] font-[500] rounded-[8px] text-[#3D3E40] flex items-center text-[16px] mr-1 hover:bg-[#F5DEB3] w-[156px] h-[51px] mr-auto ml-auto mt-[35px]"
+              class="bg-[#FF912380] pl-[19px] py-[16px] font-[500] rounded-[8px] text-[#3D3E40] flex items-center text-[16px] hover:bg-[#F5DEB3] w-[156px] h-[51px] mr-auto ml-auto mt-[35px] text-center"
             >
               Запланировать
             </button>
