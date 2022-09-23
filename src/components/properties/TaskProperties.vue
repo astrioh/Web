760209a2b93d8e2b8128ed3b0e64825b82fad522
Maslov Dmitry <63534992+devmaslove@users.noreply.github.com>
2022-09-23 12:42:09 +0300

    Update TaskProperties.vue
    fix краша при промотке сообщений

--- src/components/properties/TaskProperties.vue
+++ src/components/properties/TaskProperties.vue
@@ -577,9 +577,15 @@ export default {
       this.selectedTask.name = taskName
     },
     scrollToBottom () {
+      // TODO: нужно переписать этот кусок
+      // не должен искать по имени класса - сделать ref или еще
+      // как-то по нормальному
       this.$nextTick(() => {
         const messages = document.getElementsByClassName('messages')
-        messages[messages.length - 1].scrollIntoView(false)
+        if (messages) {
+          const lastMessage = messages[messages.length - 1]
+          if (lastMessage) lastMessage.scrollIntoView(false)
+        }
       })
     },
     createChecklist () {
