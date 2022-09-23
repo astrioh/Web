9d2419f8e1af26aa4ee45fd99b53906ef588e1fc
Maslov Dmitry <63534992+devmaslove@users.noreply.github.com>
2022-09-23 15:41:31 +0300

    Подменю доски по дизайну

---
+++ src/components/AsideMenu/SubmenuListItem.vue
@@ -0,0 +1,49 @@
+<template>
+  <div
+    class="flex items-center h-[40px] cursor-pointer hover:bg-[#f4f5f7] hover:rounded-[6px]"
+    :class="{'bg-[#f4f5f7] rounded-[6px] text-[#424242]': selected, 'text-[#606061]': !selected}"
+  >
+    <div class="flex-none flex items-center justify-center h-[30px] w-[30px] ml-[5px] mr-[8px] relative">
+      <div
+        v-if="count > 0"
+        class="min-w-[13px] min-h-[13px] absolute rounded-[3px] bg-[#4c4c4d] -top-0.5 -left-0.5 flex items-center justify-center"
+      >
+        <span
+          class="text-white font-['Roboto'] text-[10px] leading-[12px] font-medium px-0.5"
+        >
+          {{ count }}
+        </span>
+      </div>
+      <slot />
+    </div>
+    <div class="flex-initial flex items-center overflow-hidden w-full mr-[8px]">
+      <p
+        class="font-roboto text-[13px] leading-[15px] font-medium truncate"
+      >
+        {{ title }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    count: {
+      type: Number,
+      default: 0
+    },
+    selected: {
+      type: Boolean,
+      default: false
+    },
+    title: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
