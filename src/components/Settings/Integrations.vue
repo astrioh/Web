c702cb80d696455719839303e81a62be580b7393
altumus <jojojopa@icloud.com>
2022-09-26 18:48:19 +0300

    добавлена инструкция по интеграции

--- src/components/Settings/Integrations.vue
+++ src/components/Settings/Integrations.vue
@@ -9,7 +9,7 @@
     class="pt-[8px]"
     title="Интеграции"
   />
-  <div class="w-full p-10 h-full bg-white rounded-[8px]">
+  <div class="w-full px-10 py-5 h-auto bg-white rounded-[8px]">
     <div class="flex w-[300px] justify-center flex-col">
       <div class="flex items-center">
         <img
@@ -25,6 +25,72 @@
         Интеграция
       </button>
     </div>
+    <article class="mt-[30px]">
+      <p class="font-[500]">
+        Инструкция по интеграции:
+      </p>
+      <p class="mt-[10px] font-[600] text-[18px]">
+        Шаг 1. Настройте почтовый ящик
+      </p>
+      <p class="mt-[10px]">
+        <ul class="list-inside list-decimal">
+          <li>
+            Откройте
+            <a
+              class="text-[#04b]"
+              href="https://mail.yandex.ru/?dpda=yes#setup/client"
+              target="_blank"
+            >
+              раздел «Почтовые программы»
+            </a>
+            в настройках Яндекс Почты.
+          </li>
+          <li>
+            Выберите опции
+            <span class="font-[500]">С сервера imap.yandex.ru по протоколу IMAP</span>
+            и
+            <span class="font-[500]">Пароли приложений и OAuth-токены.</span>
+          </li>
+          <li>
+            Сохраните изменения.
+          </li>
+        </ul>
+      </p>
+      <p class="mt-[10px] font-[600] text-[18px]">
+        Шаг 2. Создайте пароль приложения
+      </p>
+      <p class="mt-[10px]">
+        <ul class="list-inside list-decimal">
+          <li>
+            Откройте страницу
+            <a
+              class="text-[#04b]"
+              href="https://passport.yandex.ru/profile/"
+              target="_blank"
+            >
+              Управление аккаунтом.
+            </a>
+          </li>
+          <li>
+            В разделе
+            <span class="font-[500]">Пароли и авторизация</span>
+            выберите
+            <span class="font-[500]">Включить пароли приложений.</span>
+            Подтвердите действие и нажмите
+            <span class="font-[500]">Создать новый пароль.</span>
+          </li>
+          <li>
+            Выберите тип приложения <span class="font-[500]">Почта.</span>
+          </li>
+          <li>
+            Придумайте название пароля, например укажите название приложения, для которого вы создаете пароль. С этим названием пароль будет отображаться в списке.
+          </li>
+          <li>
+            Нажмите кнопку Создать. Пароль приложения отобразится во всплывающем окне.
+          </li>
+        </ul>
+      </p>
+    </article>
   </div>
 </template>
 <script>
