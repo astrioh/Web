490464208a3db48be5eb20bbbb0704638976d600
altumus <jojojopa@icloud.com>
2022-09-26 18:19:17 +0300

    модалка об интеграции с почтой

--- src/components/Settings/Integrations.vue
+++ src/components/Settings/Integrations.vue
@@ -1,4 +1,10 @@
 <template>
+  <IntegrationsModalBox
+    v-if="showIntegration"
+    title="Интеграция с Яндекс.Почта"
+    @cancel="showIntegration = false"
+    @save="showIntegration = false"
+  />
   <NavBar
     class="pt-[8px]"
     title="Интеграции"
@@ -12,7 +18,10 @@
         >
         <span class="ml-[10px] font-[500]">Интеграция через Яндекс.Почта</span>
       </div>
-      <button class="mt-[10px] rounded-[10px] h-[40px] text-white bg-orange-300 ">
+      <button
+        class="mt-[10px] rounded-[10px] h-[40px] text-white bg-orange-300 "
+        @click="showIntegration = true"
+      >
         Интеграция
       </button>
     </div>
@@ -20,9 +29,17 @@
 </template>
 <script>
 import NavBar from '@/components/Navbar/NavBar'
+import IntegrationsModalBox from '@/components/Settings/IntegrationsModalBox.vue'
+
 export default {
   components: {
-    NavBar
+    NavBar,
+    IntegrationsModalBox
+  },
+  data () {
+    return {
+      showIntegration: false
+    }
   }
 }
 </script>
