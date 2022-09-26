a590fe5a50d42fc61651850f088aa1b4f989be51
altumus <jojojopa@icloud.com>
2022-09-26 18:09:10 +0300

    раздел "Интеграции

---
+++ src/components/Settings/Integrations.vue
@@ -0,0 +1,28 @@
+<template>
+  <NavBar
+    class="pt-[8px]"
+    title="Интеграции"
+  />
+  <div class="w-full p-10 h-full bg-white rounded-[8px]">
+    <div class="flex w-[300px] justify-center flex-col">
+      <div class="flex items-center">
+        <img
+          class="h-[30px] w-[30px]"
+          src="@/assets/images/yPochta.png"
+        >
+        <span class="ml-[10px] font-[500]">Интеграция через Яндекс.Почта</span>
+      </div>
+      <button class="mt-[10px] rounded-[10px] h-[40px] text-white bg-orange-300 ">
+        Интеграция
+      </button>
+    </div>
+  </div>
+</template>
+<script>
+import NavBar from '@/components/Navbar/NavBar'
+export default {
+  components: {
+    NavBar
+  }
+}
+</script>
