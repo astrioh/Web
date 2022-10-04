20a313a01c0e462dffdc487e4057da9b43eac998
Maslov Dmitry <63534992+devmaslove@users.noreply.github.com>
2022-10-04 13:20:15 +0300

    Рефакторинг логики Doitnow
    Переписана загрузка и получение карточек в doitnow
    Сделана единая загрузка
    Наименование у переменных поставлено нормально

--- src/components/Doitnow.vue
+++ src/components/Doitnow.vue
@@ -4,13 +4,13 @@
       class="pt-[8px]"
       title="Очередь"
     />
-    <DoitnowSkeleton
-      v-if="isLoading"
-      class="ml-0 pt-[15px] z-[2] grow"
-    />
     <DoitnowOnboarding
-      v-else-if="displayModal"
+      v-if="displayModal"
       @okToModal="okToModal"
+    />
+    <DoitnowSkeleton
+      v-else-if="isLoadingDoits"
+      class="ml-0 pt-[15px] z-[2] grow"
     />
     <DoitnowLimit
       v-else-if="showLimitMessage"
@@ -135,9 +135,7 @@ import PopMenuItem from '@/components/Common/PopMenuItem.vue'
 import SetDate from '@/components/Doitnow/SetDate.vue'
 import NavBar from '@/components/Navbar/NavBar.vue'
 
-import { PUSH_COLOR } from '@/store/actions/colors'
 import { USER_VIEWED_MODAL } from '@/store/actions/onboarding.js'
-import { TASK_STATUS } from '@/constants'
 
 import DoitnowReglament from './Doitnow/DoitnowReglament.vue'
 import DoitnowLimit from '@/components/Doitnow/DoitnowLimit'
@@ -164,30 +162,23 @@ export default {
       unreadTasks: [],
       todayTasks: [],
       readyTasks: [],
-      unreadDelegateByMe: [],
-      unreadDelegateToMe: [],
-      readyTasksReaded: [],
-      readyTasksUnreaded: [],
-      openedTasks: [],
-      slidesCopy: [],
-      projectTasks: [],
-      unsortedTasks: [],
-      notifiesCopy: [],
-      tasksLoaded: false,
+      greetingSlides: [],
+      reglaments: [],
       childrens: [],
       isTaskMessagesLoading: false,
       postponeIndex: 0,
-      postponeDate: ''
+      postponeDate: '',
+      isLoadingDoits: false
     }
   },
   computed: {
     tasksCount () {
       return (
-        this.slidesCopy.length +
+        this.greetingSlides.length +
         this.unreadTasks.length +
         this.readyTasks.length +
         this.todayTasks.length +
-        this.notifiesCopy.length +
+        this.reglaments.length +
         this.cards.length
       )
     },
@@ -208,11 +199,11 @@ export default {
       }]
     },
     firstTask () {
-      if (this.slidesCopy.length && this.justRegistered) {
-        return this.slidesCopy[0]
+      if (this.greetingSlides.length) {
+        return this.greetingSlides[0]
       }
-      if (this.notifiesCopy.length) {
-        return this.notifiesCopy[0]
+      if (this.reglaments.length) {
+        return this.reglaments[0]
       }
       if (this.cards.length) {
         return this.cards[0]
@@ -227,9 +218,6 @@ export default {
         return this.todayTasks[0]
       }
       return null
-    },
-    slides () {
-      return this.$store.state.slides.slides
     },
     taskMessages () {
       return this.$store.state.taskfilesandmessages.messages
@@ -246,12 +234,6 @@ export default {
     tags () {
       return this.$store.state.tasks.tags
     },
-    isLoading () {
-      return this.$store.state.tasks.status === 'loading' || !this.isNotifiesLoaded
-    },
-    isNotifiesLoaded () {
-      return this.$store.state.notificationtasks.status === 'success'
-    },
     user () {
       return this.$store.state.user.user
     },
@@ -260,10 +242,6 @@ export default {
     },
     displayModal () {
       return !this.$store.state.onboarding?.visitedModals?.includes('doitnow') && this.$store.state.onboarding?.showModals
-    },
-    notifies () {
-      this.setNotifiesCopy(this.$store.state.notificationtasks.notificationtasks)
-      return this.$store.state.notificationtasks.notificationtasks
     },
     isSlide () {
       return this.firstTask?.mode === 'slide'
@@ -320,93 +298,60 @@ export default {
     }
   },
   mounted: function () {
-    this.$store.dispatch('NOTIFICATION_TASKS_GENERATE').then(() => {
-      this.notifiesCopy = [...this.notifies]
-    })
-    if (this.justRegistered) {
-      this.setSlidesCopy()
-    }
-    if (!this.displayModal) {
-      this.loadAllCard()
-      this.loadAllTasks()
-    }
+    this.loadAll()
     this.$store.dispatch('fullScreenToggle', 'add')
   },
   unmounted: function () {
     this.$store.dispatch('NOTIFICATION_TASKS_CLEAR')
   },
   methods: {
-    loadAllCard () {
-      this.$store.dispatch(CARD.DOITNOW_CARDS_REQUEST)
-        .then((result) => {
-          this.cards = result
-        })
-    },
-    loadAllTasks () {
-      this.$store.dispatch(TASK.DOITNOW_TASKS_REQUEST)
-        .then((result) => {
-          // сортировка непрочитанных
-          for (let i = 0; i < result[0].length; i++) {
-            // Поручено мной
-            if (result[0][i].uid_customer === this.user?.current_user_uid) {
-              this.unreadDelegateByMe.unshift(result[0][i])
-            } else {
-              // Поручено мне
-              if (result[0][i].uid_performer === this.user?.current_user_uid) {
-                this.unreadDelegateToMe.unshift(result[0][i])
-              } else {
-                // Готово к сдаче
-                if (result[0][i].status === TASK_STATUS.TASK_READY) {
-                  this.readyTasksUnreaded.push(result[0][i])
-                } else {
-                  // Доступ
-                  if (result[0][i].emails.includes(this.user?.current_user_email) || (result[0][i].uid_project !== '00000000-0000-0000-0000-000000000000')) {
-                    this.openedTasks.push(result[0][i])
-                  }
-                }
-              }
-            }
-          }
-          // Готово к сдаче
-          this.$store.dispatch(TASK.READY_FOR_COMPLITION_TASKS_REQUEST)
-            .then((resp) => {
-              if (resp.data.anothers_markers.length) {
-                this.$store.commit(PUSH_COLOR, resp.data.anothers_markers)
-              }
-              if (resp.data.anothers_tags.length) {
-                this.$store.commit(TASK.ADD_TASK_TAGS, resp.data.anothers_tags)
-              }
-              for (let i = 0; i < resp.data.tasks.length; i++) {
-                if (resp.data.tasks[i].readed) {
-                  this.readyTasksReaded.push(resp.data.tasks[i])
-                }
-              }
-            })
-            .then(() => {
-              // Отправляем в главный массив (непрочитанное) отсортированные массивы по очереди
-              this.unreadTasks = [...this.unreadDelegateByMe, ...this.unreadDelegateToMe,
-                ...this.readyTasksUnreaded, ...this.projectTasks, ...this.unsortedTasks]
-              // Отправляем в главный массив (готовые) отсортированные данные
-              this.readyTasks = [...this.readyTasksReaded]
-              this.todayTasks = [...result[2]]
-              this.openedTasks = [...this.openedTasks]
-              // удаляем из массивов задачи со статусом "завершено"
-              this.unreadTasks = this.unreadTasks.filter(task => (task.status !== TASK_STATUS.TASK_COMPLETED) && (task.status !== TASK_STATUS.TASK_REJECTED))
-              this.readyTasks = this.readyTasks.filter(task => (task.status !== TASK_STATUS.TASK_COMPLETED) && (task.status !== TASK_STATUS.TASK_REJECTED))
-              this.todayTasks = this.todayTasks.filter(task => (task.status !== TASK_STATUS.TASK_COMPLETED) && (task.status !== TASK_STATUS.TASK_REJECTED))
-              this.openedTasks = this.openedTasks.filter(task => (task.status !== TASK_STATUS.TASK_COMPLETED) && (task.status !== TASK_STATUS.TASK_REJECTED))
-            })
-        })
-        .then(() => {
-          this.tasksLoaded = true
-        })
-    },
-    setSlidesCopy () {
-      for (let i = 0; i < this.slides.length; i++) {
-        if (!!this.slides[i].visible && (new Date(this.slides[i].reminder) <= new Date())) {
-          this.slidesCopy.push(this.slides[i])
+    async loadAll () {
+      this.isLoadingDoits = true
+      //
+      try {
+      // получаем карточки
+        this.cards = await this.$store.dispatch(CARD.DOITNOW_CARDS_REQUEST)
+        // ждем пока сгенерируются регламенты
+        // потом их получаем
+        await this.$store.dispatch('NOTIFICATION_TASKS_GENERATE')
+        this.reglaments = [...this.$store.state.notificationtasks.notificationtasks]
+        // получаем слайды (только если мы только что зарегились)
+        if (this.justRegistered) {
+          const currDate = new Date()
+          const storeSlides = this.$store.state.slides.slides
+          this.greetingSlides = storeSlides.filter(slide => !!slide.visible && (new Date(slide.reminder) <= currDate))
         }
+        // получаем задачи
+        const [tasksUnread, tasksOwerdue, tasksToday, tasksReady] = await this.$store.dispatch(TASK.DOITNOW_TASKS_REQUEST)
+        const currentUserUid = this.user?.current_user_uid
+        const unreadDelegateByMe = tasksUnread.filter(task => task.uid_customer === currentUserUid)
+        const unreadDelegateToMe = tasksUnread.filter(task => task.uid_customer !== currentUserUid && this.uid_performer === currentUserUid)
+        // заполняем главные массивы задач очереди
+        this.unreadTasks = [
+        // в массив не попали непрочитанные расшаренные мне задачи
+        // только задачи с которыми я связан как заказчик или исполнитель
+          ...unreadDelegateByMe,
+          ...unreadDelegateToMe
+        ]
+        this.readyTasks = [...tasksReady]
+        this.todayTasks = [
+          ...tasksOwerdue, // просроченные
+          ...tasksToday // сегодня
+        ]
+      } catch (e) {
+        // очищаем все массивы, как буд-то очередь пустая
+        // (по хорошему нужно сделать на экране сообщение - не удалось загрузить, кнопка попробовать еще раз)
+        this.greetingSlides = []
+        this.unreadTasks = []
+        this.readyTasks = []
+        this.todayTasks = []
+        this.reglaments = []
+        this.cards = []
+        // репортим ошибку
+        console.log('Doitnow loadAll error', e)
       }
+      //
+      this.isLoadingDoits = false
     },
     pad2 (n) {
       return (n < 10 ? '0' : '') + n
@@ -465,26 +410,22 @@ export default {
     setPostponeDate (date) {
       this.postponeDate = date
     },
-    setNotifiesCopy () {
-      this.notifiesCopy = this.notifies
-    },
     okToModal () {
       this.$store.commit(USER_VIEWED_MODAL, 'doitnow')
-      this.loadAllTasks()
     },
     readTask () {
-      this.$store.dispatch(TASK.CHANGE_TASK_READ, this.firstTask.uid)
+      this.$store.dispatch(TASK.CHANGE_TASK_READ, this.firstTask?.uid)
     },
     nextTask () {
       // Удаляем поппер 'Отложить', т.к из-за него ломается анимация
       document.querySelectorAll('.popper').forEach(popper => popper.remove())
 
-      if (this.slidesCopy.length && this.justRegistered) {
-        this.slidesCopy.shift()
+      if (this.greetingSlides.length) {
+        this.greetingSlides.shift()
         return
       }
-      if (this.notifiesCopy.length) {
-        this.notifiesCopy.shift()
+      if (this.reglaments.length) {
+        this.reglaments.shift()
         return
       }
       if (this.cards.length) {
@@ -502,10 +443,6 @@ export default {
       }
       if (this.todayTasks.length) {
         this.todayTasks.shift()
-        return
-      }
-      if (this.openedTasks.length) {
-        this.openedTasks.shift()
       }
     },
     changeValue (objWithValues) {
